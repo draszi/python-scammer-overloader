203a1e4d56dbe49b039cc634fc2a3ca58110f3e2
drazhi <drazhi@live.ca>
2018-09-08 13:41:47 -0400

    Create scam.py

---
+++ scam.py
@@ -0,0 +1,25 @@
+import requests
+import os
+import random
+import string
+import json
+
+chars = string.ascii_letters + string.digits + '!@#$S%^&*()'
+random.seed = (os.random(1024)
+
+url = 'insert_scammer_url'
+
+names = json.loads(open('names.json').read())
+
+for name in names:
+  name_number = ''.join(random.choice(string.digits))
+  
+   username = name.lower() + name_extra + '@hotmail.com'
+   password = ''.join(random.choice(chars) for i in range(9))
+   
+   requests.post(url, allow_redirets=False, data={
+    #these values are retrieved from browser
+    'some_id': some_name,
+    'some_other_id': some_other_name
+   })
+   
